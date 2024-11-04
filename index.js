9e01f37669ca82508c0788f07882e3848aa142e9
enrilpz04 <enriquelopezgordillo@gmail.com>
2024-11-04 12:42:03 +0100

    server creation

---
+++ index.js
@@ -0,0 +1,24 @@
+// Server creation and configuration
+const http = require('http');
+const app = require('./src/app');
+
+// Config .env
+require('dotenv').config();
+
+// Config DB
+require('./src/config/db');
+
+// Server creation
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT);
+
+// Listeners
+server.on('listening', () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
+server.on('error', (error) => {
+    console.log(error);
+});

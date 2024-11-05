7165f2bae438bef48fc3b059b41123afc6cec18b
mlpz <miguelkrasnob@gmail.com>
2024-11-05 08:43:05 +0100

    Agregada dependencia CORS al proyecto y usadas las opciones de cors en app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 
+const corsOption = {
+    credentials: true,
+    origin: ['http://localhost:4200']
+}
 const server = express();
 server.use(express.json());
+server.use(cors(corsOption));
 
 const rutasUsuarios = require('./routes/usuarios.route');
 server.use('/api/user', rutasUsuarios);
 
-server.listen(3000, ()=>console.log('Servidor iniciado en http://localhost:3000/api/user y conectado a la base de datos en http://localhost:3000/api/user?email=EMAIL&password=PASSWORD'));
+server.listen(3000, ()=>console.log('Servidor iniciado en http://localhost:3000/'));

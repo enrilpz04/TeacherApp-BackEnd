6b405044758d2032d01ba21331a7ac6cb3e4ad50
enrilpz04 <enriquelopezgordillo@gmail.com>
2024-11-15 13:29:18 +0100

    Login with JWT and register

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+router.use('/auth', require('./api/auth.routes'));
 router.use('/teachers', require('./api/teachers.routes'));
 
 module.exports = router;

44f41e52d93476dacbfa94deeef83416e9205dce
mlpz2 <miguelkrasnob@gmail.com>
2024-11-25 14:11:16 +0100

    Fixed messages API and SQL logic

--- src/controllers/messages.controller.js
+++ src/controllers/messages.controller.js
@@ -1,23 +1,31 @@
 const { Message, User } = require('../models');
+const sequelize = require('../config/db.js');
+const { Op } = require('sequelize');
 
 // Método para obtener todos los mensajes entre emisor y receptor (conversación)
-const getAllMessagesBetweenSenderAndRecipient = async (req, res) => {
-  const { user1 } = req.query.user1;
-  const { user2 } = req.query.user2;
+const getAllMessagesBetweenUsers = async (req, res) => {
+  const user1Id = req.query.user1Id;
+  const user2Id = req.query.user2Id;
   try {
     const messages = await Message.findAll({
       where: {
         [Op.or]: [
-          { recipientId: user1, senderId: user2 },
-          { recipientId: user2, senderId: user1 }
+          { recipientId: user1Id, senderId: user2Id },
+          { recipientId: user2Id, senderId: user1Id }
         ] },
       include: [
         {
           model: User,
           as: 'sender',
           attributes: ['id', 'name', 'surname', 'email', 'rol']
+        },
+        {
+          model: User,
+          as: 'recipient',
+          attributes: ['id', 'name', 'surname', 'email', 'rol']
         }
-      ]
+      ],
+      order: [['date', 'DESC']]
     });
     res.status(200).json(messages);
   } catch (error) {
@@ -26,24 +34,21 @@ const getAllMessagesBetweenSenderAndRecipient = async (req, res) => {
 };
 
 // Método para obtener el último mensaje de cada conversación de un usuario
-const getLastMessagesByRecipient = async (req, res) => {
-  const { user } = req.query.user;
+const getLastMessagesByUser = async (req, res) => {
+  const userId = req.query.userId;
   try {
-    const latestMessages = await Message.findAll({      
-      where: { 
-        [Op.or]: [
-          { recipientId: user },
-          { senderId: user }
-        ]
-       },
-      attributes: [
-        'text', 'senderId', 'recipientId'
-        [Sequelize.fn('MAX', Sequelize.col('date')), 'latestDate']
-      ],
-      order: [['latestDate', 'DESC']],
-      group: ['senderId', 'recipientId'],
-      raw: true
-    });
+    const latestMessages = await sequelize.query(
+      `SELECT * FROM messages
+      WHERE messages.id IN (
+        SELECT MAX(id) FROM messages
+        WHERE recipientId = 2
+          OR senderId = 2
+        GROUP BY (senderId + recipientId))
+        ORDER BY DATE DESC`,
+      {
+        model: Message,
+        mapToModel: true
+      });
     res.status(200).json(latestMessages);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -80,8 +85,8 @@ const deleteMessage = async (req, res) => {
 }
 
 module.exports = {
-  getAllMessagesBetweenSenderAndRecipient,
-  getLastMessagesByRecipient,
+  getAllMessagesBetweenUsers,
+  getLastMessagesByUser,
   createMessage,
   deleteMessage
 };

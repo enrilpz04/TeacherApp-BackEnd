87df5a387389403b2c03c6af38d103b6c039f733
enrilpz04 <enriquelopezgordillo@gmail.com>
2024-12-05 13:53:16 +0100

    notification route

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -7,5 +7,6 @@ router.use('/students', require('./api/students.routes'));
 router.use('/messages', require('./api/messages.routes'));
 router.use('/bookings', require('./api/bookings.routes'));
 router.use('/reviews', require('./api/reviews.routes'));
+router.use('/notifications', require('./api/notifications.routes'));
 
 module.exports = router;
